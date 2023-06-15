332228e3db5de58d20d00e1e302e33f6a4d41072
Daizman <andihell@yandex.ru>
2023-06-15 11:05:49 +0400

    add my longestSubstringWithoutRepeatingCharacters solution

--- cs/LeetCode/Problems/3.LongestSubstringWithoutRepeatingCharacters.cs
+++ cs/LeetCode/Problems/3.LongestSubstringWithoutRepeatingCharacters.cs
@@ -2,9 +2,37 @@ namespace LeetCode.Problems.LongestSubstringWithoutRepeatingCharacters;
 
 public class Solution
 {
+    // " ", "dvdf", "abcabcbb"
     public int LengthOfLongestSubstring(string s)
     {
-        List<char> template = new(s[0]);
-        return 0;
+        HashSet<char> template = new();
+        int startPos = 0;
+        int endPos = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (template.Contains(s[i]))
+            {
+                while (startPos < endPos)
+                {
+                    char charToRemove = s[startPos];
+                    startPos++;
+                    template.Remove(charToRemove);
+                    if (charToRemove == s[i])
+                    {
+                        break;
+                    }
+                }
+            }
+
+            template.Add(s[i]);
+            endPos = i + 1;
+            maxLength = endPos - startPos > maxLength
+                ? endPos - startPos
+                : maxLength;
+        }
+
+        return maxLength;
     }
 }
