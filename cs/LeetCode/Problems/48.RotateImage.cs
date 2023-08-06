954e373df438707d69d4e68e5a7ca2eaa883a82c
Daizman <andihell@yandex.ru>
2023-08-06 10:47:21 +0400

    optimzed rotateImage solution

--- cs/LeetCode/Problems/48.RotateImage.cs
+++ cs/LeetCode/Problems/48.RotateImage.cs
@@ -4,46 +4,19 @@ public class Solution
 {
     public void Rotate(int[][] matrix) 
     {
-        if (!matrix.Any())
-        {
-            return;
-        }
-        // Mirror matrix by vertical middle line
+        // Order important
+        Transpose(matrix);
         Mirror(matrix);
-
-        // Mirror matrix by left bottom corner to right top corner line
-        int col1 = 0, row1 = matrix.Length - 1;
-        int col2 = 0, row2 = matrix.Length - 1;
-        while (col1 != matrix.Length - 1 || col2 != matrix.Length - 1
-            || row1 != 0 || row2 != 0)
+    }
+    
+    private void Transpose(int[][] matrix)
+    {
+        for(int i = 0; i < matrix.Length; i++)
         {
-            int pointer1Col = col1, pointer1Row = row1;
-            int pointer2Col = col2, pointer2Row = row2;
-
-            while(pointer1Col < pointer2Col && pointer2Row > pointer1Row)
+            for (int j = 0; j <= i; j++)
             {
-                (matrix[pointer1Row][pointer1Col], matrix[pointer2Row][pointer2Col]) = 
-                (matrix[pointer2Row][pointer2Col], matrix[pointer1Row][pointer1Col]);
-                pointer1Col++;
-                pointer1Row++;
-                pointer2Col--;
-                pointer2Row--;
-            }
-            if (row1 > 0)
-            {
-                row1--;
-            }
-            else
-            {
-                col1++;
-            }
-            if (col2 < matrix.Length - 1)
-            {
-                col2++;
-            }
-            else
-            {
-                row2--;
+                (matrix[i][j], matrix[j][i]) =
+                (matrix[j][i], matrix[i][j]);
             }
         }
     }
@@ -52,11 +25,7 @@ public class Solution
     {
         for (int i = 0; i < matrix.Length; i++)
         {
-            for(int j = 0; j < matrix.Length / 2; j++)
-            {
-                (matrix[i][matrix.Length - 1 - j], matrix[i][j]) 
-                = (matrix[i][j], matrix[i][matrix.Length - 1 - j]);
-            }
+            Array.Reverse(matrix[i]);
         }
     }
 }
